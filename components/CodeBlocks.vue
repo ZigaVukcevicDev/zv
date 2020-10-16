543de329ef8964d8e64a36d8fb9c826fe1abf020
Ziga Vukcevic <ziga.vukcevic@comtrade.com>
2020-10-16 17:53:58 +0200

    fix: add client-only

--- components/CodeBlocks.vue
+++ components/CodeBlocks.vue
@@ -19,15 +19,15 @@
         <c-tab-panel v-for="content in contents" :key="content.id">
           <c-box margin-top="5">
             <client-only>
-              <div v-show="content.id === 'cms'">
+              <div v-if="content.id === 'cms'">
                 <c-text as="p" margin-bottom="5">Input fields in CMS</c-text>
-                <c-image v-if="content.panel[0]" :src="content.panel[0]" />
+                <c-image :src="content.panel[0]" />
                 <c-text as="p" margin-top="5" margin-bottom="5"
                   >Script and configuration
                 </c-text>
                 <nuxt-content :document="content.panel[1]" />
               </div>
-              <div v-show="content.id !== 'cms'">
+              <div v-if="content.id !== 'cms'">
                 <nuxt-content :document="content.panel" />
               </div>
             </client-only>
